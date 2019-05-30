0f3631db0348c6edffe56ac2aa4a72b69de83ee5
LzrCampos <lazarocampos98@gmail.com>
2019-05-29 22:44:11 -0300

    add saudedao

--- controller/saudeController.php
+++ controller/saudeController.php
@@ -1,11 +1,10 @@
 <?php
-    echo 'oi';
-    //include('../model/saudeDao.php');
+    include('../model/saudeDao.php');
 
-    //$btnBuscar = $_POST['buscar'];
-    //$doenca = new saudeDao();
+    $btnBuscar = $_POST['buscar'];
+    $doenca = new saudeDao();
 
-    //if ($btnBuscar == "buscar") {
-     //   $doenca->listar($_POST['slcSintoma1'], $_POST['slcSintoma2'], $_POST['slcSintoma3']);
-    //}
+    if ($btnBuscar == "buscar") {
+       $doenca->listar($_POST['slcSintoma1'], $_POST['slcSintoma2'], $_POST['slcSintoma3']);
+    }
 ?>
